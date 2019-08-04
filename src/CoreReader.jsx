d7966ef2030eca6c00f2e7754548162f325b3172
Jozef Harag <32jojo32@gmail.com>
2019-08-04 10:48:36 +0200

    Connect PDFViewer to global store

--- src/CoreReader.jsx
+++ src/CoreReader.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import Header from 'components/header/Header'
 import PDFViewer from 'components/pdf-viewer/PDFViewer'
+import GlobalProvider from 'store/GlobalProvider'
 
 import 'components/bootstrap/bootstrap.scss'
+import 'CoreReader.scss'
 
 export default class CoreReader extends React.Component {
   componentDidMount() {}
 
   render() {
     return (
-      <div>
-        <Header />
-        <PDFViewer />
-      </div>
+      <GlobalProvider>
+        <div>
+          <Header />
+          <div className="main-area">
+            <PDFViewer pdfUrl="https://arxiv.org/pdf/1907.11915.pdf" />
+          </div>
+        </div>
+      </GlobalProvider>
     )
   }
 }
--- src/components/pdf-viewer/PDFViewer.jsx
+++ src/components/pdf-viewer/PDFViewer.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import Spinner from 'reactstrap/es/Spinner'
+import pdfjs from 'pdfjs-dist/webpack'
+import PropTypes from 'prop-types'
+import withAppContext from 'store/withAppContext'
 
 import 'components/pdf-viewer/PDFViewer.scss'
 
-class PDFViewer extends React.Component {
-  componentDidMount() {}
+class PDFViewer extends React.PureComponent {
+  async componentDidMount() {
+    /**
+     * Load PDF when this component is inserted into the DOM
+     */
+    const { pdfUrl, context } = this.props
+    const pdfDocument = await pdfjs.getDocument(pdfUrl).promise
+
+    context.setPDFDocument(pdfDocument)
+  }
 
   render() {
     return (
@@ -15,4 +26,10 @@ class PDFViewer extends React.Component {
   }
 }
 
-export default PDFViewer
+PDFViewer.propTypes = {
+  pdfUrl: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  context: PropTypes.object.isRequired, // TODO: Type it better
+}
+
+export default withAppContext(PDFViewer)
