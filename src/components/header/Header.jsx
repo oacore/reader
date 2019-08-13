ea4f80f070ce5a431ea866aa508d0906ae106220
Jozef Harag <32jojo32@gmail.com>
2019-08-13 20:48:28 +0200

    Make Header as func component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,42 +4,38 @@ import Icon from 'components/icons/Icon'
 
 import 'components/header/Header.scss'
 
-class Header extends React.Component {
-  componentDidMount() {}
-
-  render() {
-    return (
-      <Navbar light color="light" className="header" tag="header">
-        <div className="item d-flex justify-content-start">
-          <button type="button" className="btn p-0 mr-3">
-            <Icon iconType="outline" />
-          </button>
-          <button type="button" className="btn p-0 mr-3">
-            <Icon iconType="thumbnails" />
-          </button>
-          <button type="button" className="btn p-0">
-            <Icon iconType="paper_info" />
-          </button>
-        </div>
-        <div className="item d-flex justify-content-center">
-          <button type="button" className="btn p-0">
-            <Icon iconType="info" />
-          </button>
-        </div>
-        <div className="item d-flex justify-content-end">
-          <button type="button" className="btn p-0">
-            <Icon iconType="share" />
-          </button>
-          <button type="button" className="btn p-0 ml-3">
-            <Icon iconType="download" />
-          </button>
-          <button type="button" className="btn p-0 ml-3">
-            <Icon iconType="print" />
-          </button>
-        </div>
-      </Navbar>
-    )
-  }
+export const Header = () => {
+  return (
+    <Navbar light color="light" className="header" tag="header">
+      <div className="item d-flex justify-content-start">
+        <button type="button" className="btn p-0 mr-3">
+          <Icon iconType="outline" />
+        </button>
+        <button type="button" className="btn p-0 mr-3">
+          <Icon iconType="thumbnails" />
+        </button>
+        <button type="button" className="btn p-0">
+          <Icon iconType="paper_info" />
+        </button>
+      </div>
+      <div className="item d-flex justify-content-center">
+        <button type="button" className="btn p-0">
+          <Icon iconType="info" />
+        </button>
+      </div>
+      <div className="item d-flex justify-content-end">
+        <button type="button" className="btn p-0">
+          <Icon iconType="share" />
+        </button>
+        <button type="button" className="btn p-0 ml-3">
+          <Icon iconType="download" />
+        </button>
+        <button type="button" className="btn p-0 ml-3">
+          <Icon iconType="print" />
+        </button>
+      </div>
+    </Navbar>
+  )
 }
 
 export default Header
