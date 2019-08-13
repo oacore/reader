6c76b20c920a34035b8e1e1a5018d66465acc34c
Jozef Harag <32jojo32@gmail.com>
2019-08-13 20:48:51 +0200

    Connect header to global state

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Navbar } from 'reactstrap'
 import Icon from 'components/icons/Icon'
+import GlobalContext from 'store/configureContext'
 
 import 'components/header/Header.scss'
 
 export const Header = () => {
+  // eslint-disable-next-line no-unused-vars
+  const context = useContext(GlobalContext)
+
   return (
     <Navbar light color="light" className="header" tag="header">
       <div className="item d-flex justify-content-start">
