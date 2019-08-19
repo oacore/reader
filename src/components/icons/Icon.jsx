d3be1d68c1b9f7b80aae1b53146682b6579a9594
Jozef Harag <32jojo32@gmail.com>
2019-08-19 18:52:16 +0200

    Add left & right icons

--- src/components/icons/Icon.jsx
+++ src/components/icons/Icon.jsx
@@ -9,6 +9,8 @@ import PaperInfoIcon from 'components/icons/assests/paper_info.svg'
 import InfoIcon from 'components/icons/assests/info.svg'
 import PrintIcon from 'components/icons/assests/print.svg'
 import ShareIcon from 'components/icons/assests/share.svg'
+import LeftArrowIcon from 'components/icons/assests/left-arrow.svg'
+import RightArrowIcon from 'components/icons/assests/right-arrow.svg'
 
 const Icon = React.memo(({ iconType, isActive = false }) => {
   // `key` property has to be specified otherwise component
@@ -74,6 +76,24 @@ const Icon = React.memo(({ iconType, isActive = false }) => {
           className={`reader-icon ${isActive ? 'active' : ''}`}
         />
       )
+    case 'left-arrow':
+      return (
+        <SVG
+          key={uniqueHash}
+          src={LeftArrowIcon}
+          className={`reader-icon ${isActive ? 'active' : ''}`}
+        />
+      )
+
+    case 'right-arrow':
+      return (
+        <SVG
+          key={uniqueHash}
+          src={RightArrowIcon}
+          className={`reader-icon ${isActive ? 'active' : ''}`}
+        />
+      )
+
     default:
       return null
   }
