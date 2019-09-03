1691c4184faa3076e389c304d741235dc4172adf
Joozty <32jojo32@gmail.com>
2019-09-03 09:47:33 +0200

    Add PDF metadata (#18)
    * Install React Helmet
    
    * Add PDF metadata

--- src/standalone.jsx
+++ src/standalone.jsx
@@ -6,9 +6,14 @@ render(
   <CoreReader
     pdfId={159107963}
     pdfUrl="https://core.ac.uk/download/pdf/159107963.pdf"
+    pdfTitle="Mining Scholarly Publications for Research Evaluation"
+    pdfAbstract="Abstract"
     publisher="Open University"
     year="2018"
     additionalInfo="Proceedings of the 16th ACM/IEEE-CS Joint Conference on Digital Libraries"
+    authors={['Author 1', 'Author 2']}
+    identifier="oai:oro.open.ac.uk:55421"
+    subject="Subject"
   />,
   document.getElementById('CORE_READER')
 )
