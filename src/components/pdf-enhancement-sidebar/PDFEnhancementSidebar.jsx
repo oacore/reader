8c31a7c9f543bc92be18696ef7b514e4383d7d82
Jozef Harag <32jojo32@gmail.com>
2019-09-28 13:36:18 +0200

    Sort annotations by vertical and horizontal position

--- src/components/pdf-enhancement-sidebar/PDFEnhancementSidebar.jsx
+++ src/components/pdf-enhancement-sidebar/PDFEnhancementSidebar.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { sortBy } from 'lodash'
 import './PDFEnhancementSidebar.scss'
 import GlobalContext from 'store/configureContext'
 
@@ -7,19 +8,29 @@ const PDFEnhancementSidebar = () => {
     state: { isEnhancementViewVisible, annotations },
   } = useContext(GlobalContext)
 
+  const sortedAnnotations = sortBy(annotations, [
+    annotation => sortBy(Object.keys(annotation.rects))[0],
+    annotation =>
+      annotation.rects[sortBy(Object.keys(annotation.rects))[0]][0].top,
+    annotation =>
+      annotation.rects[sortBy(Object.keys(annotation.rects))[0]][0].left,
+  ])
+
   return (
     <div
       className="pdf-enhancement-sidebar"
       style={{ visibility: isEnhancementViewVisible ? 'visible' : 'hidden' }}
     >
-      {Object.entries(annotations).map(([annotationId, annotationContent]) => (
-        <div
-          key={annotationId}
-          className={`info-box info-box-${annotationContent.color} p-2`}
-        >
-          {annotationContent.selectedText}
-        </div>
-      ))}
+      {Object.entries(sortedAnnotations).map(
+        ([annotationId, annotationContent]) => (
+          <div
+            key={annotationId}
+            className={`info-box info-box-${annotationContent.color} p-2`}
+          >
+            {annotationContent.selectedText}
+          </div>
+        )
+      )}
     </div>
   )
 }
