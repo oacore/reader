90cb28c33250d1f28eecd45faf5f8069c0fc942c
Jozef Harag <32jojo32@gmail.com>
2019-11-08 18:48:19 +0100

    Polyfill IE & Edge

--- polyfills.js
+++ polyfills.js
@@ -1,4 +1,8 @@
 // Support for IE
 // https://stackoverflow.com/a/53332776/5594539
+import smoothscroll from 'smoothscroll-polyfill'
+
 if (window.NodeList && !NodeList.prototype.forEach)
   NodeList.prototype.forEach = Array.prototype.forEach
+
+smoothscroll.polyfill()
