ab8f0e1a10c7c940cbcb98c11198b001231a059b
Jozef Harag <32jojo32@gmail.com>
2019-11-11 12:50:27 +0100

    Move comments closer to related code

--- polyfills.js
+++ polyfills.js
@@ -1,7 +1,7 @@
-// Support for IE
-// https://stackoverflow.com/a/53332776/5594539
 import smoothscroll from 'smoothscroll-polyfill'
 
+// Support for IE
+// https://stackoverflow.com/a/53332776/5594539
 if (window.NodeList && !NodeList.prototype.forEach)
   NodeList.prototype.forEach = Array.prototype.forEach
 
