1a1caecccb6e3381a1d0ac861d90ce550fca4b79
Joozty <32jojo32@gmail.com>
2020-01-13 15:23:02 +0100

    Track API call event

--- utils/analytics.jsx
+++ utils/analytics.jsx
@@ -3,13 +3,28 @@ import ReactGA from 'react-ga'
 
 const isProduction = process.env.NODE_ENV === 'production' || true
 
-if (!isProduction && GA_TRACKING_CODE) ReactGA.initialize(GA_TRACKING_CODE)
+if (isProduction && GA_TRACKING_CODE) ReactGA.initialize(GA_TRACKING_CODE)
 
 const logPageView = () => {
   ReactGA.set({ page: window.location.pathname })
   ReactGA.pageview(window.location.pathname)
 }
 
+export const logEvent = ({
+  category,
+  action,
+  label,
+  value,
+  nonInteraction = true,
+}) => {
+  ReactGA.event({
+    category,
+    action,
+    label,
+    value,
+    nonInteraction,
+  })
+}
 const withGoogleAnalytics = Page => {
   class WithAnalytics extends React.Component {
     // eslint-disable-next-line class-methods-use-this
