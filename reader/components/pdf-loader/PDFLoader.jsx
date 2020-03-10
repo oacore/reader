e4dd6d460c4927d13914ed27a89cd4e7872d6274
Joozty <32jojo32@gmail.com>
2020-03-10 16:57:49 +0100

    Immediately redirect when PDF is served over insecure http connection

--- reader/components/pdf-loader/PDFLoader.jsx
+++ reader/components/pdf-loader/PDFLoader.jsx
@@ -8,6 +8,9 @@ import './PDFLoader.scss'
 const CMAP_URL = 'https://cdn.jsdelivr.net/npm/pdfjs-dist@2.2.228/cmaps/'
 const CMAP_PACKED = true
 
+const redirect = url => {
+  if (url) setTimeout(() => window.location.replace(url), 5000)
+}
 const PDFLoader = React.memo(({ url, children }) => {
   const [documentProxy, setDocumentProxy] = useState(null)
   const [isLoading, setIsLoading] = useState(true)
@@ -16,12 +19,19 @@ const PDFLoader = React.memo(({ url, children }) => {
   useEffect(() => {
     const loadPDFDocument = async () => {
       try {
-        const document = await pdfjs.getDocument({
-          url,
-          cMapUrl: CMAP_URL,
-          cMapPacked: CMAP_PACKED,
-        }).promise
-        setDocumentProxy(document)
+        // it's forbidden to load http content over https
+        // browser will block it
+        if (url.startsWith('http://')) {
+          setIsLoading(false)
+          redirect(url)
+        } else {
+          const document = await pdfjs.getDocument({
+            url,
+            cMapUrl: CMAP_URL,
+            cMapPacked: CMAP_PACKED,
+          }).promise
+          setDocumentProxy(document)
+        }
       } catch (e) {
         console.error(
           `Unable to load PDF document. Redirecting to default browser view. Error: ${e}`
@@ -31,7 +41,7 @@ const PDFLoader = React.memo(({ url, children }) => {
           scope.setFingerprint(['redirection'])
           Sentry.captureException(e)
         })
-        if (url) setTimeout(() => window.location.replace(url), 5000)
+        redirect(url)
       } finally {
         setIsLoading(false)
       }
