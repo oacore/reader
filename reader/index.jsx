f752dacf14380130aeb99cef67abd3fa2e39bfff
Jozef Harag <32jojo32@gmail.com>
2020-06-09 12:26:49 +0200

    Partially remove bootstrap (#190)

--- reader/index.jsx
+++ reader/index.jsx
@@ -5,7 +5,6 @@ import { EventBus, PDFLinkService } from 'pdfjs-dist/es5/web/pdf_viewer'
 import MainArea from './components/main-area'
 import Layout from './components/layout'
 import Header from './components/header'
-import './components/bootstrap/bootstrap.scss'
 import GlobalStore from './store'
 
 const CoreReader = ({ id, downloadUrl, repositories, year }) => {
