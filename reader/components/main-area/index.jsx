0021683d6dd7f88edc185990741ba694b39e70d5
Jozef Harag <32jojo32@gmail.com>
2020-06-09 14:29:45 +0200

    Rewrite MainArea to pure CSS

--- reader/components/main-area/index.jsx
+++ reader/components/main-area/index.jsx
@@ -7,7 +7,7 @@ import OutlineSidebar from '../outline-sidebar'
 import { useGlobalStore } from '../../store'
 import { setDocument } from '../../store/document/actions'
 
-import './styles.module.scss'
+import './styles.module.css'
 
 const MainArea = () => {
   const [{ metadata, ui, document }, dispatch] = useGlobalStore()
