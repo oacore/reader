4b96e86f37e8608c790f45a0a6f4a4f49f07d161
Jozef Harag <32jojo32@gmail.com>
2020-06-11 13:19:40 +0200

    Remove next-images

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import 'pdfjs-dist/es5/web/pdf_viewer.css'
 import '@oacore/design/lib/index.css'
+import './pdf-js.css'
 import './styles.css'
 
 const App = ({ Component, pageProps }) => <Component {...pageProps} />
