55f00a29c6bebdc7efc9fad8f681a5da7a9cd521
joozty <32jojo32@gmail.com>
2021-04-04 08:37:16 +0200

    Add wiki layer

--- reader/components/wikify/index.jsx
+++ reader/components/wikify/index.jsx
@@ -4,7 +4,7 @@ import tippy from 'tippy.js'
 import styles from './styles.module.css'
 
 function getAnnotations(text) {
-  return fetch('http://localhost:8000/api/v1/wikify', {
+  return fetch('http://localhost:9999/api/v1/wikify', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -17,22 +17,100 @@ const Wikify = ({ eventBus, children }) => {
   useEffect(() => {
     eventBus.on('textlayerrendered', async (d) => {
       const annotations = await getAnnotations(
-        d.source.textContentItemsStr.join()
+        d.source.textContentItemsStr.join('')
       )
 
-      return
-      const ranges = annotations.map((spanAnnotation, spanindex) => {
-        const span = d.source.textDivs[spanindex]
-        // console.log(d)
-        return spanAnnotation.map((a) => {
-          const range = document.createRange()
-          range.setStart(span.firstChild, a.position[0])
-          range.setEnd(span.firstChild, a.position[1])
-          return [range, a.data]
-        })
-      })
+      const ranges = []
 
-      ranges.flat().forEach(([range, data]) => {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const annotation of annotations) {
+        let oldlen = 0
+        let newlen = 0
+        for (let i = 0; i < d.source.textContentItemsStr.length; i++) {
+          oldlen = newlen
+          newlen += d.source.textContentItemsStr[i].length
+
+          // found the start of the entity
+          if (
+            annotation.position[0] < newlen
+            // d.source.textDivs[i].firstChild !== null
+          ) {
+            // entity is within current span
+            const range = document.createRange()
+            const startIndex = Math.max(oldlen, annotation.position[0])
+            const start = startIndex - oldlen
+            const end = Math.min(
+              annotation.position[1] - startIndex + start,
+              d.source.textContentItemsStr[i].length
+            )
+
+            range.setStart(d.source.textDivs[i].firstChild, start)
+            range.setEnd(d.source.textDivs[i].firstChild, end)
+            ranges.push([range, annotation.data])
+
+            if (annotation.position[1] - newlen <= 0) break
+
+            // // entity multiple spans
+            // if (
+            //   annotation.position[0] <= newlen &&
+            //   annotation.position[1] > newlen &&
+            //   !foundStart
+            // ) {
+            //   const range = document.createRange()
+            //   range.setStart(d.source.textDivs[i].firstChild, 0)
+            //   range.setEnd(d.source.textDivs[i].firstChild, newlen -  annotation.position[0] )
+            //   ranges.push([range, annotation.data])
+            //   foundStart = true
+            // }
+            //
+            // // entity withing multiple spans
+            // if (
+            //   annotation.position[1] > newlen &&
+            //   !foundStart &&
+            //   d.source.textDivs[i].firstChild
+            // ) {
+            //   const range = document.createRange()
+            //   range.setStart(d.source.textDivs[i].firstChild, 0)
+            //   range.setEnd(
+            //     d.source.textDivs[i].firstChild,
+            //     d.source.textContentItemsStr[i].length
+            //   )
+            //   ranges.push([range, annotation.data])
+            // }
+
+            //   else {
+            //     try {
+            //       const range = document.createRange()
+            //       range.setStart(
+            //         d.source.textDivs[i].firstChild,
+            //         len - annotation.position[0]
+            //       )
+            //       range.setEnd(
+            //         d.source.textDivs[i].firstChild,
+            //         d.source.textContentItemsStr[i].length
+            //       )
+            //       ranges.push([range, annotation.data])
+            //     } catch (e) {
+            //       console.log(
+            //         annotation,
+            //         len,
+            //         d.source.textContentItemsStr[i].length,
+            //         len -
+            //           d.source.textContentItemsStr[i].length -
+            //           annotation.position[0]
+            //       )
+            //     }
+            //   }
+            // }
+
+            // if(len <=)
+          }
+
+          // console.log(value)
+        }
+      }
+
+      ranges.forEach(([range, data]) => {
         const mark = document.createElement('mark')
 
         mark.classList.add(styles.wikiAnnotation)
