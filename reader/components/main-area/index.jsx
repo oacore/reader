d9950cc9dfbbb80479bbbe4784fe116ccee03657
ekachxaidze98 <65679299+ekachxaidze98@users.noreply.github.com>
2023-12-06 13:55:37 +0400

    CORE-4567: reader logo section (#335)

--- reader/components/main-area/index.jsx
+++ reader/components/main-area/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { classNames } from '@oacore/design/lib/utils'
 
-import Viewer from '../viewer'
 import PdfLoader from '../pdf-loader/pdf-loader'
 import ThumbnailSidebar from '../thumbnails-sidebar'
 import OutlineSidebar from '../outline-sidebar'
 import { useGlobalStore } from '../../store'
 import { setDocument } from '../../store/document/actions'
 import styles from './styles.module.css'
+import Viewer from '../viewer/index'
 
-const MainArea = () => {
+const MainArea = ({ members }) => {
   const [{ metadata, ui, document }, dispatch] = useGlobalStore()
 
   return (
@@ -31,6 +31,7 @@ const MainArea = () => {
           metadata={metadata}
           setDocument={(d) => dispatch(setDocument(d))}
           globalDispatch={dispatch}
+          members={members}
         />
       </PdfLoader>
     </div>
