c98a1d86deac7cc14741f4d145c2a33ba287c28e
anwiev <anwiev@gmail.com>
2025-09-03 19:21:50 +0300

    added static

--- design.config.js
+++ design.config.js
@@ -26,7 +26,7 @@ const config = {
 
   output: {
     path: path.join(__dirname, 'public/design'),
-    publicPath: '/design',
+    publicPath: '/static/design',
     icons: {
       files: 'icons',
       sprite: 'icons.svg',
