95ed54cd34e1b8d8cb1cc47b0b17087298cef501
anwiev <anwiev@gmail.com>
2025-09-03 19:28:55 +0300

    add reader/design

--- design.config.js
+++ design.config.js
@@ -26,7 +26,7 @@ const config = {
 
   output: {
     path: path.join(__dirname, 'public/design'),
-    publicPath: '/static/design',
+    publicPath: '/reader/design',
     icons: {
       files: 'icons',
       sprite: 'icons.svg',
