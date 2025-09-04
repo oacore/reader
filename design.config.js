6f208b2ef7e3ffe02a65c9c9b8abf62c4d136796
anwiev <anwiev@gmail.com>
2025-09-04 11:47:45 +0300

    leve only design path

--- design.config.js
+++ design.config.js
@@ -26,7 +26,7 @@ const config = {
 
   output: {
     path: path.join(__dirname, 'public/design'),
-    publicPath: '/reader/design',
+    publicPath: '/design',
     icons: {
       files: 'icons',
       sprite: 'icons.svg',
