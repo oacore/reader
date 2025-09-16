26ddffd4a6d7ab392d3369a330f88a8cab20e746
mcancellieri <mcancellieri@users.noreply.github.com>
2025-09-16 14:24:36 +0100

    Update design.config.js icons url

--- design.config.js
+++ design.config.js
@@ -26,7 +26,7 @@ const config = {
 
   output: {
     path: path.join(__dirname, 'public/design'),
-    publicPath: '/design',
+    publicPath: '/static/reader',
     icons: {
       files: 'icons',
       sprite: 'icons.svg',
