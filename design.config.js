b20616de02bfd439a3eae4058459fea82aa631c4
ekachxaidze98 <ekachxaidze2@gmail.com>
2025-09-22 15:57:13 +0400

    fix icon path

--- design.config.js
+++ design.config.js
@@ -26,7 +26,7 @@ const config = {
 
   output: {
     path: path.join(__dirname, 'public/design'),
-    publicPath: '/static/reader',
+    publicPath: '/static/design',
     icons: {
       files: 'icons',
       sprite: 'icons.svg',
